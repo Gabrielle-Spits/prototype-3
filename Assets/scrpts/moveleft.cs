fd9f9da7d4cd613583312865f57e122b68c57a5c
Gabrielle Spits <gabrielle-spits@hotmail.com>
2019-12-02 11:14:27 +0100

    no message

---
+++ Assets/scrpts/moveleft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveleft : MonoBehaviour
+{
+    private float speed = 30;
+    private playercontroler playerControllerScript;
+    private float leftBound = -15;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("player").GetComponent<playercontroler>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound)
+        {
+            
+        }
+    }
+}
